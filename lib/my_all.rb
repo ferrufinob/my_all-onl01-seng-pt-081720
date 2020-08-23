984971018d889b830095216543c02d9967f9e66e
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-23 02:51:20 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -6,7 +6,13 @@ def my_all?(collection)
   while i < collection.length
  block_return_values << yield(collection[i])
   i = i + 1
-  
+end
+
+if block_return_values.include?(false)
+  false
+else
+  true
+end
 end
 end
 my_all?([1,2,3]) {|i| i < 2}
